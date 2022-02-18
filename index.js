bd7d8a40943d32761239a668a4a4accfef26b06a
Giancarlo Martini <giancarlomartini@gmail.com>
2022-02-18 13:25:14 +0100

    Riformattato il codice

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-/*
- index.js
- ============================================
+/***************************************************************************
+	progetto			: "Raspberry Temperature Show"
+    file:				: index.js
+    begin               : mer apr 21 10:34:57 CET 2011
+    copyright           : (C) 2011 by Giancarlo Martini
+    email               : gm@giancarlomartini.it
+ ***************************************************************************/
 
- Copyright (c) 2016-2017 by  Giancarlo Martini
- http://www.giancarlomartini.it
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
- This program is free software. You can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License.
-**********************************************************************
-
-*/
 $( document ).ready(temperature_request());
 //------------------------------------------------------
-function temperature_request()
-{
+function temperature_request() {
 	/*var text_to_send = $('#text_to_send_id').val();
 	if(text_to_send.length == 0) return;;
 
@@ -24,22 +27,19 @@ function temperature_request()
 	window.alert("Vars:" + data_to_send);*/
 	var data_to_send = $.param({'temp':"giveme"});
 	$.ajax({url: '/cgi-bin/temperature_show.cgi', cache:false,success: tr_success, error:tr_error,
-		data:"",dataType:'text',type:'get'});
+			data:"",dataType:'text',type:'get'});
 }
 // --------------------- onFailure -------------------------------------
-function tr_error(XMLHttpRequest, textStatus, errorThrown)
-{
+function tr_error(XMLHttpRequest, textStatus, errorThrown) {
 	window.alert('ERRORE: Errore in trasmissione\n' + textStatus + ":" + errorThrown);
 }
 // --------------------- onSuccess -------------------------------------
-function tr_success(data, textStatus, XMLHttpRequest)
-{
-  var result = data.split("|");
-  if(result[0] == "OK"){
+function tr_success(data, textStatus, XMLHttpRequest) {
+	var result = data.split("|");
+	if(result[0] == "OK") {
 		//window.alert("Dati inviati correttamente\n");
 		$('#temperature_received_id').val(result[1]);
-	}
-  else window.alert("ERRORE:\n" + data);
+	} else window.alert("ERRORE:\n" + data);
 }
 //-------------------------------------------------------------------------------
 //------------------------------------------------------
--- main.c
+++ main.c
@@ -1,8 +1,22 @@
- /* main.c
- * author: 5 ELE
- * date: 12/01/2017
- * description: main.c
- */
+/***************************************************************************
+	progetto			: "Raspberry Temperature Show"
+    file:				: main.c
+    begin               : mer apr 21 10:34:57 CET 2011
+    copyright           : (C) 2011 by Giancarlo Martini
+    email               : gm@giancarlomartini.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -18,69 +32,66 @@ void read_serial_value(int ttys_descriptor,char *value);
 int get_value(const char *device, char *buffer);
 const char DEVICE[] = "/dev/ttyUSB0";
 
-int main(int argc, char *argv[])
-{
-    char value[10];
-    printf("Content-Type:text/html;charset=utf8\n\n");
-    // Legge un valore, in caso di errore il msg lo invia prima
+int main(int argc, char *argv[]) {
+	char value[10];
+	printf("Content-Type:text/html;charset=utf8\n\n");
+	// Legge un valore, in caso di errore il msg lo invia prima
 	if(get_value(DEVICE,value) == true)	printf("OK|%s",value);
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
-int get_value(const char *device, char *buffer)
-{
+int get_value(const char *device, char *buffer) {
 
-   // Prova ad aprire il device/porta seriale
+	// Prova ad aprire il device/porta seriale
 	int ttys_descriptor = rs232_open(device,B9600,CS8,PNONE,SB1);
-    if(ttys_descriptor == -1){
+	if(ttys_descriptor == -1) {
 		perror("ERROR|SERIAL ERROR:Port not open!!!\nSerial port is:");
 		printf("%s\n",device);
 		return false;
 	}
-    // Elimina eventuali dati parziali
-    empty_serial_buffer(ttys_descriptor);
-    read_serial_value(ttys_descriptor,buffer);
+	// Elimina eventuali dati parziali
+	empty_serial_buffer(ttys_descriptor);
+	read_serial_value(ttys_descriptor,buffer);
 	rs232_close(ttys_descriptor);
 	return true;
 }
-void empty_serial_buffer(int ttys_descriptor)
-{
-    char buffer[10];
-    int bytes;
+void empty_serial_buffer(int ttys_descriptor) {
+	char buffer[10];
+	int bytes;
 	//fflush(ttys_descriptor);
-    while(1) {
-        bytes = rs232_bytes_arrived(ttys_descriptor,2);
-        if(bytes <= 0) continue;
-        rs232_read(ttys_descriptor,buffer,1);
-        if(buffer[0] != '\n') continue;
-        // I prossimi dati sono completi
-        break;
-    }
+	while(1) {
+		bytes = rs232_bytes_arrived(ttys_descriptor,2);
+		if(bytes <= 0) continue;
+		rs232_read(ttys_descriptor,buffer,1);
+		if(buffer[0] != '\n') continue;
+		// I prossimi dati sono completi
+		break;
+	}
 }
 
-void read_serial_value(int ttys_descriptor,char *value)
-{
-    int bytes, pos = 0;char buffer[10];
+void read_serial_value(int ttys_descriptor,char *value) {
+	int bytes, pos = 0;
+	char buffer[10];
 	char dummy[1000];
-    while(1) {
-        bytes = rs232_bytes_arrived(ttys_descriptor,2);
-        if(bytes <= 0) continue;
+	while(1) {
+		bytes = rs232_bytes_arrived(ttys_descriptor,2);
+		if(bytes <= 0) continue;
 
-        rs232_read(ttys_descriptor,buffer,1);
-        // Controlla se è arrivato il fine valore
-        if(buffer[0] == '\n'){
-            value[pos] = '\0';
-            return;
-        }
+		rs232_read(ttys_descriptor,buffer,1);
+		// Controlla se è arrivato il fine valore
+		if(buffer[0] == '\n') {
+			value[pos] = '\0';
+			return;
+		}
 		if(isdigit(buffer[0]) == 0) continue;
-        value[pos] = buffer[0];
-        pos++;
-        if(pos > MAX_LEN_VALUE){
-        	value[pos] = '\0';
-            //printf("Anomalia in ricezione, valore troppo lungo:%s\nAspetto il successivo",value);
-            pos = 0;
-            empty_serial_buffer(ttys_descriptor);
-        }
+		value[pos] = buffer[0];
+		pos++;
+		if(pos > MAX_LEN_VALUE) {
+			value[pos] = '\0';
+			//printf("Anomalia in ricezione, valore troppo lungo:%s\nAspetto il successivo",value);
+			pos = 0;
+			empty_serial_buffer(ttys_descriptor);
+		}
 
-    }
+	}
 }
